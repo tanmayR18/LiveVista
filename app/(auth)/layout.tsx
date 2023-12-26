2fc121bca9ef333b12eceab8328e49a561a83a15
tanmayrane18 <tanmayrane51@gmail.com>
2023-12-26 16:21:51 +0530

    login-signup and webhooks added

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,10 +1,13 @@
+import { Logo } from "./_components/logo";
+
 const AuthLayout = ({
     children
 }: {
     children: React.ReactNode
 }) => {
     return ( 
-        <div className=" flex justify-center items-center h-full">
+        <div className=" flex flex-col gap-2 justify-center items-center h-full ">
+            <Logo/>
             {children}
         </div>
     );
