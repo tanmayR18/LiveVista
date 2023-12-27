64a8d248b9eab3d5f74c1b53676307d0acc0935e
tanmayrane18 <tanmayrane51@gmail.com>
2023-12-27 09:16:30 +0530

    Navbar added

--- app/(browse)/(home)/page.tsx
+++ app/(browse)/(home)/page.tsx
@@ -4,9 +4,7 @@ import { UserButton } from '@clerk/nextjs'
 export default function Home() {
   return (
     <div>
-        <UserButton 
-        afterSignOutUrl='/'
-        />
+        <h1>Home page</h1>
     </div>
   )
 }
